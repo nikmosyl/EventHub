cb774d24936622b71712bd236f15c365fbd63081
nikita <nikmosyl@gmail.com>
2025-09-07 22:29:36 +0400

    Initial Commit

---
+++ EventHub/EventHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHubApp.swift
+//  EventHub
+//
+//  Created by nikita on 07.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct EventHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
