5f00b119ededc4ab5920fcc1f26817a741228b9c
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2025-09-10 15:05:08 +0500

    build up a sign up screen

---
+++ EventHub/AuthModule/SignUp/SignUpView.swift
@@ -0,0 +1,99 @@
+//
+//  SignUpView.swift
+//  EventHub
+//
+//  Created by Mikhail Ustyantsev on 10.09.2025.
+//
+
+import SwiftUI
+
+enum AuthRoute: Hashable {
+    case signIn
+    case signUp
+}
+
+
+struct SignUpView: View {
+    
+    /// View Properties
+    @State private var fullName: String = ""
+    @State private var emailID: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    
+    /// Navigation
+    @State private var path = NavigationPath()
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color("background")
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .center, spacing: 50) {
+                    /// Custom Text Fields
+                    CustomTF(sfIcon: "person", hint: "Full name", value: $fullName)
+                    
+                    CustomTF(sfIcon: "envelope", hint: "abc@email.com", value: $emailID)
+                    
+                    CustomTF(sfIcon: "lock", hint: "Your password", isPassword: true, value: $password)
+                    
+                    CustomTF(sfIcon: "lock", hint: "Confirm password", isPassword: true, value: $confirmPassword)
+                    
+                    VStack(alignment: .center, spacing: 40) {
+                        SignInButton(
+                            backgroundColor: .buttonPrimary,
+                            circleColor: .buttonSecondary,
+                            title: "SIGN UP") {
+                                //handle sign up action
+                                
+                            }
+                            .frame(width: 300)
+                            .buttonStyle(PrimaryButtonStyle(height: 58, cornerRadius: 16))
+                                .filledButtonBackground(.buttonPrimary)
+                                .foregroundStyle(.white)
+                        
+                        Text("OR")
+                            .foregroundStyle(.secondary)
+                        
+                        GoogleButton(
+                            title: "Login with Google",
+                            image: "googleIcon",
+                            action: {
+                                //Login with Google Auth Service
+                                
+                            }
+                        )
+                            .frame(width: 300)
+                            .buttonStyle(PrimaryButtonStyle(height: 58, cornerRadius: 16))
+                            .filledButtonBackground(Color.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .stroke(Color.secondary.opacity(0.2), lineWidth: 1)
+                            )
+                            .foregroundStyle(.black)
+                    }
+                    
+                    HStack {
+                        Text("Already have an account?")
+                        
+                        Button(action: {
+                            //handle sign in action
+                        }) {
+                            Text("Sign in")
+                                .foregroundColor(Color.buttonPrimary)
+                        }
+                    }
+                }
+                .padding(.horizontal, 25)
+            }
+            .navigationTitle("Sign up")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+}
+
+#Preview {
+    SignUpView()
+}
