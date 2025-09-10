d9824bdb22d40475229fc991df4217d72faf6f39
Artem Tarasov <artemdrolllted@gmail.com>
2025-09-10 15:51:38 +0300

    draw version

--- EventHub/Modules/ExploreScreen/ExploreMain/ExploreView.swift
+++ EventHub/Modules/ExploreScreen/ExploreMain/ExploreView.swift
@@ -12,7 +12,7 @@ struct ExploreView: View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 0) {
-                    ZStack {
+                    ZStack(alignment: .top) {
                         Rectangle()
                             .fill(Color.navBarPrimary)
                             .frame(height: 200)
@@ -21,6 +21,7 @@ struct ExploreView: View {
                                                       corners: [.bottomLeft, .bottomRight])
                             )
                             .ignoresSafeArea(.all)
+                            .offset(y: -65)
                         
                         VStack(spacing: 20) {
                             CityLocationView()
@@ -33,66 +34,66 @@ struct ExploreView: View {
                             }
                             .padding(.horizontal, 20)
                             
-
-                                //.padding(.horizontal, 20)
-                        }
-                        .padding(.top, 50)
-                    }
-                    .offset(y: -65)
-                    
-                    VStack(spacing: 20) {
-                        
-                        VariableSectionView()
-                        
-                        HStack {
-                            Text("Upcoming Events")
-                                .font(.system(size: 18, weight: .bold))
-                                .foregroundColor(.black)
+                            VariableSectionView()
+                                .padding(.leading, 20)
+                                .offset(y: 10)
                             
-                            Spacer()
+                            VariableSectionView()
+                                .padding(.leading, 20)
+                                .offset(y: 10)
                             
-                            Text("See All")
-                                .font(.system(size: 14))
-                                .foregroundColor(.gray)
-                        }
-                        .padding(.horizontal, 20)
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 15) {
-                                EventsCard()
-                                EventsCard()
-                                EventsCard()
+                            VStack(spacing: 20) {
+                                HStack {
+                                    Text("Upcoming Events")
+                                        .font(.system(size: 18, weight: .bold))
+                                        .foregroundColor(.black)
+                                    
+                                    Spacer()
+                                    
+                                    Text("See All")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.gray)
+                                }
+                                .padding(.horizontal, 20)
+                                
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 15) {
+                                        EventsCard()
+                                        EventsCard()
+                                        EventsCard()
+                                    }
+                                    .padding(.horizontal, 20)
+                                }
+                                
+                                HStack {
+                                    Text("Nearby You")
+                                        .font(.system(size: 18, weight: .bold))
+                                        .foregroundColor(.black)
+                                    
+                                    Spacer()
+                                    
+                                    Text("See All")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.gray)
+                                }
+                                .padding(.horizontal, 20)
+                                
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 15) {
+                                        EventsCard()
+                                        EventsCard()
+                                    }
+                                    .padding(.horizontal, 20)
+                                }
                             }
-                            .padding(.horizontal, 20)
-                        }
-                        
-                        HStack {
-                            Text("Nearby You")
-                                .font(.system(size: 18, weight: .bold))
-                                .foregroundColor(.black)
-                            
-                            Spacer()
-                            
-                            Text("See All")
-                                .font(.system(size: 14))
-                                .foregroundColor(.gray)
-                        }
-                        .padding(.horizontal, 20)
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 15) {
-                                EventsCard()
-                                EventsCard()
-                            }
-                            .padding(.horizontal, 20)
+                            .padding(.top, 20)
+                            .background(Color.white)
                         }
                     }
-                    .padding(.top, 20)
-                    .background(Color.white)
                 }
             }
+            .toolbar(.hidden, for: .navigationBar)
         }
-        .toolbar(.hidden, for: .navigationBar)
     }
 }
 
