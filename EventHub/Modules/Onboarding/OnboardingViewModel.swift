2aea20473b4664766a810909e7b219c99fb7bb3b
Анастасия <nastasiatihonovaa@gmail.com>
2025-09-14 14:27:31 +0300

    фиксы после ревью

---
+++ EventHub/Modules/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingViewModel.swift
+//  EventHub
+//
+//  Created by Анастасия Тихонова on 14.09.2025.
+//
+
+import SwiftUI
+
+class OnboardingViewModel: ObservableObject {
+    @Published var currentStepIndex: Int = 0
+    let steps: [OnboardingData] = OnboardingData.steps
+    
+    var currentStep: OnboardingData {
+        steps[currentStepIndex]
+    }
+    
+    func nextStep() {
+        if currentStepIndex < steps.count - 1 {
+            currentStepIndex += 1
+        } else {
+            print("goToNext")
+        }
+    }
+    func skipOnboarding() {
+        print("skipOnboarding")
+    }
+}
